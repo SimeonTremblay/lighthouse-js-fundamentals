e80d3d9cfbd41d2cddf57e30b9317338272c6c37
Simeon <simeontremblay@gmail.com>
2021-03-03 21:02:27 +0000

    Use a loop to repeat a chorus line

--- conditionals.js
+++ conditionals.js
@@ -24,8 +24,21 @@ console.log("Now you're ready to go outside!");
 
 
 const isCitizen = true;
-const age = 26;
+let age = 26;
 
 if (isCitizen && age > 18) {
   console.log("You are eligable");
 }
+ 
+
+const whichSchool  = function (age) {
+  if (age < 13){
+    return ("Elementary School");
+  } else if (age >= 13 && age <= 18) {
+    return ("Secondary School"); 
+  } else {
+    return("Lighthouse Labs");
+  }
+}
+console.log(whichSchool(15));
+
